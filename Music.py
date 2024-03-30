e7e845aa5a2ee3a89a1499a394f10872b96bce34
Luhan Salimena <luhan@beepayapp.com.br>
2024-03-30 19:11:54 -0300

    Add Music class and create instances

---
+++ Music.py
@@ -0,0 +1,19 @@
+class Music:
+    name = ''
+    artist = ''
+    duration = 0
+
+musica1 = Music()
+musica1.nome = 'Bohemian Rhapsody'
+musica1.artista = 'Queen'
+musica1.duracao = 355
+
+musica2 = Music()
+musica2.nome = 'Imagine'
+musica2.artista = 'John Lennon'
+musica2.duracao = 183
+
+musica3 = Music()
+musica3.nome = 'Shape of You'
+musica3.artista = 'Ed Sheeran'
+musica3.duracao = 234
